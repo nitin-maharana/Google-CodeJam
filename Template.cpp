e249c78eb587a12a2fe1bf8d5efa8eb0fb60d56d
Nitin Kumar Maharana <nitin.maharana@gmail.com>
2016-05-01 01:44:43 +0530

    Google Code Jam Template(For writing code)

---
+++ Template.cpp
@@ -0,0 +1,34 @@
+/*
+ * Written by Nitin Kumar Maharana
+ * nitin.maharana@gmail.com
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <list>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+	int t;
+
+	cin >> t;
+
+	for(int i = 1; i <= t; i++)
+	{
+		cout << "Case #" << i << ": ";
+	}
+
+	return 0;
+}
